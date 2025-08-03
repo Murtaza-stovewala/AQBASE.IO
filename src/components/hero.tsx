905ceab58e26799a81e79603f9bbd05e76f4eea5
Murtaza Stove <akmurtaza7@gmail.com>
2025-08-03 15:28:31 +0000

    add a backgroun like our service in hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <section id="home" className="w-full py-20 md:py-32 lg:py-40">
+    <section id="home" className="w-full py-20 md:py-32 lg:py-40 bg-secondary">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center text-center">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-4">
-                <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-400 font-headline">
+                <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-foreground to-muted-foreground font-headline">
                     Crafting Scalable Web & App Experiences
                 </h1>
                 <p className="max-w-[600px] mx-auto text-muted-foreground md:text-xl">
